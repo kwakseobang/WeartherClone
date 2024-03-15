db642631f0cc180a97e791639c9dd2d9c34f5b40
Kwakmunsu <iii6602@naver.com>
2024-03-15 13:51:02 +0900

    first commit

---
+++ Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by 곽서방 on 3/15/24.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
